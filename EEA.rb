bc7c39d6fd9a0bd7d66fac6d2819b8ed2fbf9c6d
Kevin Peng <kpeng97@gmail.com>
2016-03-15 01:53:30 -0400

    added dynamic resizing

--- EEA.rb
+++ EEA.rb
@@ -1,8 +1,7 @@
-$WIDTH = 6
-def pretty_print(*args)
+def pretty_print(width, *args)
   output = ""
   args.each do |a|
-    output << a.to_s.rjust($WIDTH)
+    output << a.to_s.rjust(width)
   end
   puts output
 end
@@ -14,16 +13,17 @@ puts "Extended Euclidean Algorithm, Step by Step by Kevin Peng."
 a = ARGV[0].to_i
 b = ARGV[1].to_i
 a,b = b,a if b> a
+width = a.to_s.length + 2
 q1, q2, x1, y1, x2, y2, r1 = 0, 0, 1, 0, 0, 1, a
-pretty_print "x", "y", "r", "q"
-puts "----" * $WIDTH
-pretty_print x1, y1, r1, q1
+pretty_print width, "x", "y", "r", "q"
+puts "----" * width
+pretty_print width, x1, y1, r1, q1
 r2 = b
-pretty_print x2, y2, r2, q2
+pretty_print width, x2, y2, r2, q2
 while r2>0 do
   q1, q2 = q2, r1/r2
   r1, r2 = r2, r1%r2
   x1, x2 = x2, x1-q2*x2
   y1, y2 = y2, y1-q2*y2
-  pretty_print x2, y2, r2, q2
+  pretty_print width, x2, y2, r2, q2
 end
